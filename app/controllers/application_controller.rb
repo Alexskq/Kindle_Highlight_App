4c4e533c086fd6ebf1dd0b9a3a4db27454a0a709
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-01-06 17:05:25 +0100

    check app_cont.rb comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token 
+  # ! need to be check
+
+  
+  protect_from_forgery with: :exception, except: [:verify_authenticity_token] 
+  #skip_before_action :verify_authenticity_token 
   before_action :authenticate_user!
 
 end
